d9506d0baaeddf3d46e7b2aff89be957d4ad2fe7
Samantha <samantha@dbas-Mac-mini.local>
2022-11-18 12:30:32 -0800

    added all 9 hours to timeblock

---
+++ script.js
@@ -0,0 +1,10 @@
+//add dayjs to add current date and time
+//have timeblocks with hours, area for text, and button to save to local storage. 
+//add diff colors for already happened, upcoming and current. 
+//need to be able to retrieve from local storage
+
+//9 of everything- can work with a loop to write code that affects all elements
+//hour <p> or <h3>
+//use text area for the input field-pref 
+//use button for save element 
+// querySelectorAll
